ed29e10cb501556076716c19e47f4beadc972f3a
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-11-02 16:10:01 -0500

    init create_deflated_means_table usign fnmate

---
+++ R/create_deflated_means_table.R
@@ -0,0 +1,15 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##'
+##' @title
+##' @param microdata
+##' @param aux_data
+##' @param inventory
+create_deflated_means_table <- function(microdata = microdata, 
+                                        aux_data  = aux_data, 
+                                        inventory = inventory) {
+
+  NULL
+
+}
