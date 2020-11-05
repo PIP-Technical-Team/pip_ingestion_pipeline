63ff8092227bd376b8328f02fed276243bb57fde
R.Andres Castaneda <acastanedaa@worldbank.org>
2020-11-04 20:56:35 -0500

    function to join tables

---
+++ R/join_dsm_tables.R
@@ -0,0 +1,29 @@
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##'
+##' @title
+##' @param ud
+##' @param old
+join_dsm_tables <- function(ud, 
+                            old = NULL) {
+  setDT(ud)
+  
+  if (is.null(old)) {
+    df <- ud
+  } else {
+    setDT(old)
+    new_id <- ud[, .(survey_id = unique(survey_id))]
+    
+    #remove in old in case there is an update
+    old <- old[!new_id,
+               on = .(survey_id)]
+    
+    # append data
+    df <- rbindlist(list(ud, old))
+  }
+  setorder(df, country_code, year, module, vermast, veralt)
+  
+  return(df)
+
+}
