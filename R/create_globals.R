7c652ea6507a6e857a77affabcd4e3b3af8416e8
Tony Fujs <tfujs@worldbank.org>
2021-08-12 17:37:42 +0200

    fix bug in create_globals()

--- R/create_globals.R
+++ R/create_globals.R
@@ -2,22 +2,22 @@ create_globals <- function(root_dir = '//w1wbgencifs01/pip') {
   glbs <- list()
   
   # Input dir 
-  glbs$PIP_DATA_DIR     <- '//w1wbgencifs01/pip/PIP-Data_QA/' 
+  glbs$PIP_DATA_DIR     <- paste0(root_dir, '/PIP-Data_QA/') 
   
   # '//w1wbgencifs01/pip/pip_ingestion_pipeline/' # Output dir
-  glbs$PIP_PIPE_DIR     <- '//w1wbgencifs01/pip/pip_ingestion_pipeline/' 
+  glbs$PIP_PIPE_DIR     <- paste0(root_dir, '/pip_ingestion_pipeline/') 
   
   # Cached survey data dir
-  glbs$CACHE_SVY_DIR    <- paste0(PIP_PIPE_DIR, 'pc_data/cache/clean_survey_data/') 
+  glbs$CACHE_SVY_DIR    <- paste0(glbs$PIP_PIPE_DIR, 'pc_data/cache/clean_survey_data/') 
   
   # Final survey data output dir
-  glbs$OUT_SVY_DIR      <- paste0(PIP_PIPE_DIR, 'pc_data/output/survey_data/') 
+  glbs$OUT_SVY_DIR      <- paste0(glbs$PIP_PIPE_DIR, 'pc_data/output/survey_data/') 
   
   #  Estimations output dir
-  glbs$OUT_EST_DIR      <- paste0(PIP_PIPE_DIR, 'pc_data/output/estimations/') 
+  glbs$OUT_EST_DIR      <- paste0(glbs$PIP_PIPE_DIR, 'pc_data/output/estimations/') 
   
   # aux data output dir
-  glbs$OUT_AUX_DIR      <- paste0(PIP_PIPE_DIR, 'pc_data/output/aux/')  
+  glbs$OUT_AUX_DIR      <- paste0(glbs$PIP_PIPE_DIR, 'pc_data/output/aux/')  
   
   time             <- format(Sys.time(), "%Y%m%d%H%M%S") 
   
@@ -32,4 +32,6 @@ create_globals <- function(root_dir = '//w1wbgencifs01/pip') {
   glbs$PIP_REF_YEARS    <- 1981:max_year # Years used in the interpolated means table
   
   glbs$FST_COMP_LVL     <- 100 # Compression level for .fst output files
+  
+  return(glbs)
 }
