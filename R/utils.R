091e76f5a9ada99cf9a2c028bdc3d162e66f06a3
Tony Fujs <tfujs@worldbank.org>
2021-08-13 12:24:20 +0200

    delete _common.R file
    * Moved used functions to utils.R
    * Deleted unused code

--- R/utils.R
+++ R/utils.R
@@ -18,3 +18,51 @@ save_estimations <- function(dt, dir, name,
   
   return(paste0(dir, name, ".fst"))
 }
+
+create_input_folder_structure <- function(dir) {
+  
+  # Create sub directories if needed 
+  
+  # _aux folder
+  if (!dir.exists(paste0(dir, '_aux')))
+    dir.create(paste0(dir, '_aux'))
+  
+  # _inventory folder 
+  if (!dir.exists(paste0(dir, '_inventory')))
+    dir.create(paste0(dir, '_inventory'))
+  
+  # _log folders 
+  if (!dir.exists(paste0(dir, '_log')))
+    dir.create(paste0(dir, '_log'))
+  if (!dir.exists(paste0(dir, '_log/info')))
+    dir.create(paste0(dir, '_log/info'))
+  if (!dir.exists(paste0(dir, '_log/info/vintage')))
+    dir.create(paste0(dir, '_log/info/vintage'))
+  
+  # Create other files if needed 
+  
+  # Create empty _log/info .dtasig files  
+  if (!file.exists(paste0(dir, '_log/info/pip_info_gd.dtasig')))
+    file.create(paste0(dir, '_log/info/pip_info_gd.dtasig'))
+  if (!file.exists(paste0(dir, '_log/info/pip_info_md.dtasig')))
+    file.create(paste0(dir, '_log/info/pip_info_md.dtasig'))
+  
+}
+
+check_missing_input_files <- function(dir) {
+  
+  # Check that _a_ WEO.xls file is available 
+  # The most recent version of this file needs to be 
+  # manually downloaded from IMF's webpage. 
+  weo_path <- '_aux/weo/WEO_[0-9]{4}-[0-9]{2}-[0-9]{2}.xls'
+  if (!file.exists(paste0(dir, weo_path)))
+    rlang::abort('WEO_[YYYY-MM-DD].xls file not found.')
+  
+  # Check that _the_ 2021-01-14 version of NAS special.xlsx
+  # ia available. The use of this specific file is currently
+  # hardcoded in pipaux::pip_gdp_update and pipaux::pip_pce_update. 
+  nas_path <- '_aux/sna/NAS special_2021-01-14.xlsx'
+  if (!file.exists(paste0(dir, nas_path)))
+    rlang::abort('NAS special_2021-01-14.xlsx file not found.')
+  
+}
