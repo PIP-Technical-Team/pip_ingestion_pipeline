f893b260a272212f75a478bedfba5306222e9770
Andres <acastastanedaa@worldbank.org>
2021-11-29 15:44:38 -0500

    map dl_dist_stats

--- R/mapping.R
+++ R/mapping.R
@@ -12,3 +12,35 @@ mp_cache <- function(cache_dir) {
                })
   
 }
+
+
+mp_lorenz <- function(cache) {
+  d <- purrr::map(cache, db_compute_lorenz)
+  purrr::keep(d, ~!is.null(.x))
+  
+}
+
+
+
+
+
+
+mp_dl_dist_stats <- function(dt         ,
+                             mean_table ,
+                             pop_table  ,
+                             cache_id   ) {
+  
+  purrr::map2(.x = dt, 
+              .y = cache_id, 
+              .f = ~{
+                db_compute_dist_stats(dt         = .x,
+                                      mean_table = mean_table,
+                                      pop_table  = pop_table,
+                                      cache_id   = .y)
+              })
+  
+}
+
+
+
+
