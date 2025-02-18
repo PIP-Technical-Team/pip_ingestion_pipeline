2429b594ab7253f046d21eb0c30a3cfa6e2e0941
R.Andres Castaneda <acastastanedaa@worldbank.org>
2025-02-18 18:53:37 -0500

    working version of split_weights

--- R/pipdm/R/replicate_households.R
+++ R/pipdm/R/replicate_households.R
@@ -60,7 +60,7 @@ expand_outliers <- function(DT, weight = "weight", ori_names) {
 }
 
 # Wrapper Function
-process_data <- function(DT, weight = "weight", threshold = 2.5) {
+replicate_households <- function(DT, weight = "weight", threshold = 2.5) {
   DT <- copy(DT)  # work on a copy to avoid modifying original DT
   ori_names <- DT |> 
     names() |> 
@@ -78,6 +78,64 @@ DT <- data.table(
   weight = c(10, 31, 12, 9, 8, 90, 124)
 )
 
-DT_new <- process_data(DT, weight = "weight", threshold = 1)
+DT_new <- replicate_households(DT, weight = "weight", threshold = 1)
 DT_new[]
 
+
+library(ggplot2)
+
+civ <- pipload::pip_load_cache("CIV", 2002)
+ury <- pipload::pip_load_cache("PRY", 2018)
+
+
+ury  |>  
+  ggplot(aes(x = weight)) +
+    geom_histogram(bins = 100) +
+    geom_vline(aes(xintercept=fsd(weight)*2.5+fmean(weight)),
+               color="blue", linetype="dashed", linewidth=1) +
+    geom_density(alpha=.2, fill="#FF6666") 
+
+setorder(civ, welfare)
+civp <- wbpip::md_compute_quantiles(welfare = civ$welfare, 
+                                    weight = civ$weight, 
+                                    n_quantile = 100000)
+length(civp)
+any(diff(civp) < 0)
+
+
+
+nq <- 100
+
+
+dt <- civ |>
+  ## totals -----------
+fgroup_by(c("welfare_type")) |>
+  fmutate(tot_pop = fsum(weight),
+          tot_wlf = fsum(welfare_ppp*weight)) |>
+  fungroup() |>
+  setorder(welfare_type, welfare_ppp) |>
+  fgroup_by(c("welfare_type", "reporting_level")) |>
+  fmutate(bin = wbpip:::md_compute_bins(welfare = welfare_ppp,
+                         weight = weight,
+                         nbins = nq)) |>
+  fungroup() |> 
+  ## shares at the observation level ---------
+ftransform(pop_share = weight/tot_pop,
+           welfare_share = (welfare_ppp*weight)/tot_wlf) |>
+  ## aggregate
+  fgroup_by(reporting_level, welfare_type, bin) |>
+  fsummarise(avg_welfare    = fmean(welfare_ppp, w = weight),
+             pop_share      = fsum(pop_share),
+             welfare_share  = fsum(welfare_share),
+             quantile       = fmax(welfare_ppp),
+             pop            = fsum(weight)) |>
+  fungroup()
+
+perr <- which(diff(dt$welfare_share) < 0)
+if (length(perr)) {
+  dt[perr]
+}
+
+dim(civ)
+
+civ2 <- replicate_households(civ)
